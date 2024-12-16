5ac684fb4e0b6aa1f93d36a387b65bc67f02dedc
Olliver-0 <nata.gabriel.de.oliveira@gmail.com>
2024-12-16 09:58:53 -0300

    Novos exercícios

---
+++ cursopy/exercicios/ex072/tuplas.py
@@ -0,0 +1,33 @@
+# lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+# # print(lanche[1])
+# # print(lanche[-1])
+# # print(lanche[1:3])
+# # print(lanche[2:])
+# # print(lanche[:4])
+# # print(lanche)
+# # #for pos, comida in enumerate(lanche):
+# #     # print(f'Eu vou comer {comida} na posição {pos}')
+# # for cont in range(0, len(lanche)):
+# #     print(lanche[cont])
+
+# print(sorted(lanche))
+
+# a = (2, 5, 4)
+# b = (5, 8, 1, 2)
+# c = a + b
+# print(c.count(5))
+# print(c.index(8))
+# print(c.index(5, 1))
+
+# pessoa = ('Gustavo', 39, 'M', 99.88)
+# print(pessoa)
+# del(pessoa)
+
+nums = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+num = int(input('Digite um número entre 0 e 20: '))
+while num < 0 or num > 20:
+    num = int(input('Tente novamente. Digite um número entre 0 e 20: '))
+
+print(f'Você digitou o número {extenso[nums[num]]}')
---
+++ cursopy/exercicios/ex073/brasileirao.py
@@ -0,0 +1,8 @@
+times = ('Botafogo', 'Palmeiras', 'Flamengo', 'Fortaleza', 'Internacional', 'São Paulo', 'Corinthians', 'Bahia', 'Cruzeiro', 'Vasco', 'Vitória', 'Atlético-MG', 'Fluminense', 'Grêmio', 'Juventude', 'Red Bull Bragantino', 'Athletico-PR', 'Criciúma', 'Atlético-GO', 'Cuiabá')
+
+print(times[:5])
+print(times[16:20])
+print(sorted(times))
+for c, t in enumerate(times):
+    if t == 'Cruzeiro':
+        print(c + 1)
